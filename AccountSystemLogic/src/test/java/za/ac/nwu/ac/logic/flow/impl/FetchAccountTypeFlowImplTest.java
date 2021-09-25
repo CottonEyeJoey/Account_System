ab18b611d6fe1189a08ae97516645c3500878ca7
CottonEyeJoey <joedel1310@gmail.com>
2021-09-25 18:23:53 +0200

    Added project files

---
+++ AccountSystemLogic/src/test/java/za/ac/nwu/ac/logic/flow/impl/FetchAccountTypeFlowImplTest.java
@@ -0,0 +1,26 @@
+package za.ac.nwu.ac.logic.flow.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FetchAccountTypeFlowImplTest {
+
+    private FetchAccountTypeFlowImpl testClass;
+    @Before
+    public void setUp() throws Exception {
+        testClass = new FetchAccountTypeFlowImpl( null);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        testClass = null;
+    }
+
+    @Test
+    public void methodToTest() {
+        assertTrue(testClass.methodToTest());
+    }
+}
