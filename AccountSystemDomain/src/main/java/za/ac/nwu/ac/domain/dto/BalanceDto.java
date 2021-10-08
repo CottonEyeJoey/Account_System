4cac3d1d39bc06394a59a5d0eb94c68834ffbc62
CottonEyeJoey <joedel1310@gmail.com>
2021-10-08 15:58:09 +0200

    Testing changes 08 Oct 2021

--- AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/dto/BalanceDto.java
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/dto/BalanceDto.java
@@ -1,4 +1,85 @@
 package za.ac.nwu.ac.domain.dto;
 
-public class BalanceDto {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import za.ac.nwu.ac.domain.persistence.AccountType;
+import za.ac.nwu.ac.domain.persistence.Balance;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+public class BalanceDto implements Serializable {
+
+    private static final long serialVersionUID = 8732924396956507429L;
+
+    private Long memberId;
+    private String accountTypeMnemonic;
+    private Long amount;
+
+    public BalanceDto() {
+    }
+
+    public BalanceDto(Long memberId, String accountTypeMnemonic, Long amount) {
+        this.memberId = memberId;
+        this.accountTypeMnemonic = accountTypeMnemonic;
+        this.amount = amount;
+    }
+
+    public BalanceDto(Balance balance){
+        this.memberId = balance.getMemberId();
+        this.accountTypeMnemonic = balance.getAccountType().getMnemonic();
+        this.amount = balance.getAmount();
+    }
+
+    @JsonIgnore
+    public Balance buildBalance(AccountType accountType){
+        return new Balance(this.getMemberId(),accountType,this.getAmount());
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getAccountTypeMnemonic() {
+        return accountTypeMnemonic;
+    }
+
+    public void setAccountTypeMnemonic(String accountTypeMnemonic) {
+        this.accountTypeMnemonic = accountTypeMnemonic;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BalanceDto that = (BalanceDto) o;
+        return Objects.equals(memberId, that.memberId) && Objects.equals(accountTypeMnemonic, that.accountTypeMnemonic) && Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberId, accountTypeMnemonic, amount);
+    }
+
+    @Override
+    public String toString() {
+        return "BalanceDto{" +
+                "memberId=" + memberId +
+                ", accountTypeMnemonic='" + accountTypeMnemonic + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/persistence/Balance.java
@@ -0,0 +1,78 @@
+package za.ac.nwu.ac.domain.persistence;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "BALANCE", schema = "hr")
+public class Balance implements Serializable {
+
+    private static final long serialVersionUID = -3199417323867984916L;
+
+    private Long memberId;
+    private AccountType accountType;
+    private Long amount;
+
+    public Balance() {
+    }
+
+    public Balance(Long memberId, AccountType accountType,Long amount) {
+        this.memberId = memberId;
+        this.accountType = accountType;
+        this.amount = amount;
+    }
+
+    @Id
+    @Column(name = "MEMBER_ID")
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACCOUNT_TYPE_ID")
+    public AccountType getAccountType()
+    {
+        return accountType ;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    @Column(name = "AMOUNT")
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Balance that = (Balance) o;
+        return Objects.equals(memberId, that.memberId) && Objects.equals(accountType, that.accountType) && Objects.equals(amount, that.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberId, accountType,  amount);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountTransaction{" +
+                "memberId=" + memberId +
+                ", accountType=" + accountType +
+                ", amount=" + amount +
+                '}';
+    }
+}
